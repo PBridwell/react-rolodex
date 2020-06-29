a927946e8191eb23fcac4daea9329650bf93c903
P Bridwell <patbridwell@cox.net>
2020-06-28 19:57:50 -0500

    brings in overarching table and sets functions for sorting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import api from './utils/api';
+import EmployeeTable from './components/EmployeeTable/index';
 
 // main app class
 class App extends React.Component {
@@ -26,9 +27,111 @@ class App extends React.Component {
 	componentDidMount() {
 		this.searchEmployees();
 	}
+	//  sort by id function, reversing then resorting on alternating clicks
+	handleSortID = () => {
+		const idSort = this.state.employees;
+		if (!this.state.sortID) {
+			idSort.sort((a, b) => (a.id > b.id ? 1 : -1));
+			this.setState({
+				employees: idSort,
+				sortID: true,
+			});
+			// Functionality for alternate click
+		} else {
+			idSort.reverse((a, b) => (a.id < b.id ? -1 : 1));
+			this.setState({
+				employees: idSort,
+				sortID: false,
+			});
+		}
+	};
+
+	//  sort by name function, reversing then resorting on alternating clicks
+	handleSortName = () => {
+		const nameSort = this.state.employees;
+		if (!this.state.sortName) {
+			// 1 and -1 values for ascending and descending
+			nameSort.sort((a, b) => (a.name > b.name ? 1 : -1));
+			this.setState({
+				employees: nameSort,
+				sortName: true,
+			});
+		} else {
+			nameSort.reverse((a, b) => (a.name < b.name ? -1 : 1));
+			this.setState({
+				employees: nameSort,
+				sortName: false,
+			});
+		}
+	};
+
+	//  sort by email function, reversing then resorting on alternating clicks
+	handleSortEmail = () => {
+		const emailSort = this.state.employees;
+		if (!this.state.sortEmail) {
+			emailSort.sort((a, b) => (a.email > b.email ? 1 : -1));
+			this.setState({
+				employees: emailSort,
+				sortEmail: true,
+			});
+		} else {
+			emailSort.reverse((a, b) => (a.email < b.email ? -1 : 1));
+			this.setState({
+				employees: emailSort,
+				sortEmail: false,
+			});
+		}
+	};
+
+	//  sort by phone function, reversing then resorting on alternating clicks
+	handleSortPhone = () => {
+		const phoneSort = this.state.employees;
+		if (!this.state.sortPhone) {
+			phoneSort.sort((a, b) => (a.phone > b.phone ? 1 : -1));
+			this.setState({
+				employees: phoneSort,
+				sortPhone: true,
+			});
+		} else {
+			phoneSort.reverse((a, b) => (a.phone < b.phone ? -1 : 1));
+			this.setState({
+				employees: phoneSort,
+				sortPhone: false,
+			});
+		}
+	};
+
+	//  update the state.search value instantly as input is entered in form
+	handleSearch = (e) => {
+		const value = e.target.value;
+		const name = e.target.name;
+		this.setState({
+			[name]: value,
+		});
+	};
+
+	// onClick take the contents of the input and use it to filter employees returned
+	handleSubmit = (e) => {
+		e.preventDefault();
+		if (this.state.search === '') {
+			this.searchEmployees();
+		} else {
+			this.setState({
+				employees: [
+					...this.state.employees.filter((employee) =>
+						employee.name.includes(this.state.search)
+					),
+				],
+			});
+		}
+	};
 
 	render() {
-		return <div className='text-center'></div>;
+		return (
+			<div className='text-center'>
+				<EmployeeTable></EmployeeTable>
+			</div>
+		);
 	}
 }
 
---
+++ src/components/EmployeeTable/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+// setup the table headers with sort
+function EmployeeTable(props) {
+	return (
+		<table className='table'>
+			<thead className='thead-light'>
+				<tr>
+					<th scope='col'>
+						ID#{' '}
+						<i
+							onClick={props.handleSortID}
+							className='fa fa-sort'
+							aria-hidden='true'
+						></i>
+					</th>
+					<th scope='col'>
+						Name{' '}
+						<i
+							onClick={props.handleSortName}
+							className='fa fa-sort'
+							aria-hidden='true'
+						></i>
+					</th>
+					<th scope='col'>
+						Email{' '}
+						<i
+							onClick={props.handleSortEmail}
+							className='fa fa-sort'
+							aria-hidden='true'
+						></i>
+					</th>
+					<th scope='col'>
+						Phone{' '}
+						<i
+							onClick={props.handleSortPhone}
+							className='fa fa-sort'
+							aria-hidden='true'
+						></i>
+					</th>
+				</tr>
+			</thead>
+			<tbody>{props.children}</tbody>
+		</table>
+	);
+}
+
+export default EmployeeTable;
