5c3e68aa5d585493de840f17b3660c2d9b290ca9
P Bridwell <patbridwell@cox.net>
2020-07-01 09:59:06 -0500

    converts class components to functional

--- src/App.js
+++ src/App.js
@@ -1,155 +1,172 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import api from './utils/api';
+
 import EmployeeTable from './components/EmployeeTable/index';
 import SearchBar from './components/SearchBar';
 import Directory from './components/Directory';
 
 // main app class
-class App extends React.Component {
-	// states
-	state = {
-		employees: [],
-		search: '',
-		sortID: false,
-		sortName: false,
-		sortEmail: false,
-		sortPhone: false,
-	};
+// class App extends React.Component {
+// 	// states
+// 	state = {
+// 		employees: [],
+// 		search: '',
+// 		sortID: false,
+// 		sortName: false,
+// 		sortEmail: false,
+// 		sortPhone: false,
+// 	};
+
+const App = () => {
+	const [employees, setEmployees] = useState([]);
+	const [search, setSearch] = useState('');
+	const [sortID, setSortID] = useState(false);
+	const [sortName, setSortName] = useState(false);
+	const [sortEmail, setSortEmail] = useState(false);
+	const [sortPhone, setSortPhone] = useState(false);
 
 	//   calls axios search function
-	searchEmployees = () => {
+	const searchEmployees = () => {
 		api
 			.search()
-			.then((res) => this.setState({ employees: res.data }))
-			.then((res) => console.log(this.state.employees))
+			.then((res) => setEmployees(res.data))
+			.then((res) => console.log(employees))
 			.catch((err) => console.log(err));
 	};
+
 	// Perform search for all employees on page load
-	componentDidMount() {
-		this.searchEmployees();
-	}
-	//  sort by id function, reversing then resorting on alternating clicks
-	handleSortID = () => {
-		const idSort = this.state.employees;
-		if (!this.state.sortID) {
+	// componentDidMount() {
+	// 	this.searchEmployees();
+	// }
+	useEffect(() => {
+		searchEmployees();
+	}, []);
+	// //  sort by id function, reversing then resorting on alternating clicks
+	const handleSortID = () => {
+		const idSort = employees;
+		if (!sortID) {
 			idSort.sort((a, b) => (a.id > b.id ? 1 : -1));
-			this.setState({
-				employees: idSort,
-				sortID: true,
-			});
+			// seState({
+			// 	employees: idSort,
+			// 	sortID: true,
+			// });
+			setEmployees(idSort);
+			setSortID(true);
 			// Functionality for alternate click
 		} else {
 			idSort.reverse((a, b) => (a.id < b.id ? -1 : 1));
-			this.setState({
-				employees: idSort,
-				sortID: false,
-			});
+			// this.setState({
+			// 	employees: idSort,
+			// 	sortID: false,
+			// });
+			setEmployees(idSort);
+			setSortID(false);
 		}
 	};
 
-	//  sort by name function, reversing then resorting on alternating clicks
-	handleSortName = () => {
-		const nameSort = this.state.employees;
-		if (!this.state.sortName) {
-			// 1 and -1 values for ascending and descending
-			nameSort.sort((a, b) => (a.name > b.name ? 1 : -1));
-			this.setState({
-				employees: nameSort,
-				sortName: true,
-			});
-		} else {
-			nameSort.reverse((a, b) => (a.name < b.name ? -1 : 1));
-			this.setState({
-				employees: nameSort,
-				sortName: false,
-			});
-		}
-	};
+	// //  sort by name function, reversing then resorting on alternating clicks
+	// handleSortName = () => {
+	// 	const nameSort = this.state.employees;
+	// 	if (!this.state.sortName) {
+	// 		// 1 and -1 values for ascending and descending
+	// 		nameSort.sort((a, b) => (a.name > b.name ? 1 : -1));
+	// 		this.setState({
+	// 			employees: nameSort,
+	// 			sortName: true,
+	// 		});
+	// 	} else {
+	// 		nameSort.reverse((a, b) => (a.name < b.name ? -1 : 1));
+	// 		this.setState({
+	// 			employees: nameSort,
+	// 			sortName: false,
+	// 		});
+	// 	}
+	// };
 
-	//  sort by email function, reversing then resorting on alternating clicks
-	handleSortEmail = () => {
-		const emailSort = this.state.employees;
-		if (!this.state.sortEmail) {
-			emailSort.sort((a, b) => (a.email > b.email ? 1 : -1));
-			this.setState({
-				employees: emailSort,
-				sortEmail: true,
-			});
-		} else {
-			emailSort.reverse((a, b) => (a.email < b.email ? -1 : 1));
-			this.setState({
-				employees: emailSort,
-				sortEmail: false,
-			});
-		}
-	};
+	// //  sort by email function, reversing then resorting on alternating clicks
+	// handleSortEmail = () => {
+	// 	const emailSort = this.state.employees;
+	// 	if (!this.state.sortEmail) {
+	// 		emailSort.sort((a, b) => (a.email > b.email ? 1 : -1));
+	// 		this.setState({
+	// 			employees: emailSort,
+	// 			sortEmail: true,
+	// 		});
+	// 	} else {
+	// 		emailSort.reverse((a, b) => (a.email < b.email ? -1 : 1));
+	// 		this.setState({
+	// 			employees: emailSort,
+	// 			sortEmail: false,
+	// 		});
+	// 	}
+	// };
 
-	//  sort by phone function, reversing then resorting on alternating clicks
-	handleSortPhone = () => {
-		const phoneSort = this.state.employees;
-		if (!this.state.sortPhone) {
-			phoneSort.sort((a, b) => (a.phone > b.phone ? 1 : -1));
-			this.setState({
-				employees: phoneSort,
-				sortPhone: true,
-			});
-		} else {
-			phoneSort.reverse((a, b) => (a.phone < b.phone ? -1 : 1));
-			this.setState({
-				employees: phoneSort,
-				sortPhone: false,
-			});
-		}
-	};
+	// //  sort by phone function, reversing then resorting on alternating clicks
+	// handleSortPhone = () => {
+	// 	const phoneSort = this.state.employees;
+	// 	if (!this.state.sortPhone) {
+	// 		phoneSort.sort((a, b) => (a.phone > b.phone ? 1 : -1));
+	// 		this.setState({
+	// 			employees: phoneSort,
+	// 			sortPhone: true,
+	// 		});
+	// 	} else {
+	// 		phoneSort.reverse((a, b) => (a.phone < b.phone ? -1 : 1));
+	// 		this.setState({
+	// 			employees: phoneSort,
+	// 			sortPhone: false,
+	// 		});
+	// 	}
+	// };
 
-	//  update the state.search value instantly as input is entered in form
-	handleSearch = (e) => {
+	// //  update the state.search value instantly as input is entered in form
+	const handleSearch = (e) => {
 		const value = e.target.value;
-		const name = e.target.name;
-		this.setState({
-			[name]: value,
-		});
+		// const name = e.target.name;
+		// this.setState({
+		// 	[name]: value,
+		// });
+		setSearch(value);
+		console.log(search);
 	};
 
-	// onClick take the contents of the input and use it to filter employees returned
-	handleSubmit = (e) => {
-		e.preventDefault();
-		// If searchbar is empty, entire list is returned
-		if (this.state.search === '') {
-			this.searchEmployees();
-		} else {
-			this.setState({
-				employees: [
-					...this.state.employees.filter((employee) =>
-						employee.name.includes(this.state.search)
-					),
-				],
-			});
-		}
-	};
+	// // onClick take the contents of the input and use it to filter employees returned
+	// handleSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	// If searchbar is empty, entire list is returned
+	// 	if (this.state.search === '') {
+	// 		this.searchEmployees();
+	// 	} else {
+	// 		this.setState({
+	// 			employees: [
+	// 				...this.state.employees.filter((employee) =>
+	// 					employee.name.includes(this.state.search)
+	// 				),
+	// 			],
+	// 		});
+	// 	}
+	// };
 
-	render() {
-		return (
-			<div className='text-center'>
-				{/* Header searchbar here */}
-				<SearchBar
-					value={this.state.search}
-					handleSearch={this.handleSearch}
-					handleSubmit={this.handleSubmit}
-				></SearchBar>
+	return (
+		<div className='text-center'>
+			{/* Header searchbar here */}
+			<SearchBar
+				value={search}
+				handleSearch={handleSearch}
+				// handleSubmit={this.handleSubmit}
+			></SearchBar>
 
-				<EmployeeTable
-					handleSortID={this.handleSortID}
-					handleSortPhone={this.handleSortPhone}
-					handleSortEmail={this.handleSortEmail}
-					handleSortName={this.handleSortName}
-				>
-					<Directory employees={this.state.employees} />
-				</EmployeeTable>
-			</div>
-		);
-	}
-}
+			<EmployeeTable
+				handleSortID={handleSortID}
+				// handleSortPhone={this.handleSortPhone}
+				// handleSortEmail={this.handleSortEmail}
+				// handleSortName={this.handleSortName}
+			>
+				<Directory employees={employees} />
+			</EmployeeTable>
+		</div>
+	);
+};
 
 export default App;
--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import EmployeeList from '../EmployeeList/index';
 import PropTypes from 'prop-types';
 
-// reference the employlist child component cycing through all employees imported using the map function
-class Directory extends Component {
-	render() {
-		return this.props.employees.map((employee) => (
-			<EmployeeList key={employee.id} employee={employee} />
-		));
-	}
-}
+import React from 'react';
 
+const Directory = (props) => {
+	return props.employees.map((employee) => (
+		<EmployeeList key={employee.id} employee={employee} />
+	));
+};
 // proptypes
 Directory.propTypes = {
 	employees: PropTypes.array.isRequired,
