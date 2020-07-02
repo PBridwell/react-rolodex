545cfbaa54e8bcd88d0189fcdae31eb8282d2669
P Bridwell <patbridwell@cox.net>
2020-07-02 18:19:32 -0500

    Updates

---
+++ precache-manifest.733d831f67209d66c3f291006777c0d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f75c2c4db6da190ae4ce601a3a5933",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "446dc7a2435b1e5a4d01",
+    "url": "/react-rolodex/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4bca5a5dfa862ca8ee04",
+    "url": "/react-rolodex/static/js/2.5a71fe94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rolodex/static/js/2.5a71fe94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446dc7a2435b1e5a4d01",
+    "url": "/react-rolodex/static/js/main.24bcd4bc.chunk.js"
+  },
+  {
+    "revision": "67a63b1f8f40800b148d",
+    "url": "/react-rolodex/static/js/runtime-main.73ccd20c.js"
+  }
+]);
